87ebaf13d3b2b3907ed25c4c82170ae0ebfbbd4c
michaelabhi01 <47388448+michaelabhi01@users.noreply.github.com>
2020-08-26 16:08:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const jString = {};
 
-jString.host = "localhost",
-    jString.user = "root";
-jString.password = "";
-jString.database = "quiz_lolipop";
-jString.port = "";
+jString.host = "us-cdbr-east-02.cleardb.com",
+jString.user = "bdeab2e8b62093";
+jString.password = "5aa878c5";
+jString.database = "heroku_5a8923ad86155f0";
+
 
 
 exports.dbConfig = jString;

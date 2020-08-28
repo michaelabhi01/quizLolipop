60adb3f4f06c205af7def1aef66fd789eec7637d
Abhishek Singh <michaelabhi01@gmail.com>
2020-08-28 18:10:43 +0530

    updated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const port = process.env.PORT || 6000;
 const server = http.createServer(app);
@@ -10,10 +11,9 @@ const server = http.createServer(app);
 server.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({
-    extended: true,
-}));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.all("/*", function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');

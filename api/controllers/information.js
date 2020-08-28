f5ac8c8b0467256722a29981365d78670f90e4c6
Abhishek Singh <michaelabhi01@gmail.com>
2020-08-28 17:56:43 +0530

    update

--- api/controllers/information.js
+++ api/controllers/information.js
@@ -3,5 +3,5 @@ const pjson = require('../../package.json');
 exports.information = (req, res) => {
 
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`<b>Quiz lolipop API Node.js Version ${pjson.version}</b>`);
+    res.end(`<b>Quiz-lolipop API Node.js Version ${pjson.version}</b>`);
 }
